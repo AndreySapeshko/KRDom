7092207225ad3b4600a3e2cbb1d02183a23ed4c4
Andrey Sapeshko <andrdd17@gmail.com>
2026-01-22 17:36:01 +0300

    Развернут Docker и создан ci.yml

---
+++ backend/api/main.py
@@ -0,0 +1,17 @@
+import logging
+import logging.config
+
+from starlette.middleware.cors import CORSMiddleware
+
+from backend.logging_config import LOGGING_CONFIG
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="KRDom",
+    version="0.1.0",
+)
