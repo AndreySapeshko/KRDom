61e7b94c199427c16628412db0e14b1b9afba4f4
Andrey Sapeshko <andrdd17@gmail.com>
2026-02-01 09:28:27 +0300

    Изменен health, docker-compose

--- backend/api/routers/health.py
+++ backend/api/routers/health.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/", include_in_schema=False)
+@router.get("", include_in_schema=False)
 async def healthcheck():
     return {"status": "ok"}
