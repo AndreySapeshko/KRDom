3a737c0a830f56d0e788ee07c11fb522b900d66e
Andrey Sapeshko <andrdd17@gmail.com>
2026-02-02 13:32:37 +0300

    изменен verify_telegram_init_data

--- backend/api/auth/telegram_auth/dependencies.py
+++ backend/api/auth/telegram_auth/dependencies.py
@@ -23,8 +23,9 @@ def verify_telegram_init_data(init_data: str) -> dict:
     received_hash = parsed.pop("hash")
 
     data_check_string = "\n".join(f"{k}={v}" for k, v in sorted(parsed.items()))
-    secret = sha256(TELEGRAM_BOT_TOKEN.encode()).digest()
-    calculated_hash = hmac.new(secret, data_check_string.encode(), sha256).hexdigest()
+    secret_key = hmac.new(b"WebAppData", TELEGRAM_BOT_TOKEN.encode(), sha256).digest()
+
+    calculated_hash = hmac.new(secret_key, data_check_string.encode(), sha256).hexdigest()
 
     if calculated_hash != received_hash:
         print("Invalid Telegram signature")
